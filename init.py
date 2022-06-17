c45d1ab65e40679a0f04d9dff01a648c1bc9d430
SungGook Cho <sunggook.cho@theblackcardapp.com>
2022-06-17 16:30:14 +0900

    assume initial electron density, but need the meaning of exact G vector

--- init.py
+++ init.py
@@ -13,29 +13,113 @@ import random
 
 # G-space
 
-def generate_plane_wave_basis(i_max = k_max = G_max = 10):
+i_max = 1; k_max = 10; G_max = 10
+#######################################
+# i는 occupied state
+# k는 nodes의 인덱스 
+# G는 reciprocal space vector, 의미가 정확히 뭐고 수학적으로 어떻게 풀어야 하지?
+#######################################
 
-    C = np.random.random((i_max, k_max, G_max))
+
+
+C = np.random.random((i_max, k_max, G_max))
+
+
+def flatten_3d_arr(nx, ny, nz): # flattened 3d array
+    return np.zeros((nx - 1) * (ny - 1) * (nz - 1))
+
+def generate_plane_wave_basis(r = None, option="functional"):
+
+    global C, i_max, k_max, G_max
     
-    Psi = [[None for k in range(k_max)]for i in range(i_max)]
+    Psi = [[None for k in range(k_max)] for i in range(i_max)]
     
-    # r 함수형
-    for i in range(i_max):
-        for k in range(k_max):
-            def func(r, i = i, k = k):
-                global C
+    
+    if option == "functional":
+        # r 함수형
+        for i in range(i_max):
+            for k in range(k_max):
+                def func(r, i = i, k = k):
+                    global C
+                    tmp = []
+                    for G in range(G_max):                
+                        print(C[i][k][G])
+                        tmp.append(C[i][k][G] * np.exp(complex(0, (k + G) * r)))
+                    return tmp
+                Psi[i][k] = func
+    
+        return Psi
+    else:
+        for i in range(i_max):
+            for k in range(k_max):                
                 tmp = []
                 for G in range(G_max):                
                     print(C[i][k][G])
-                    tmp.append(C[i][k][G] * np.exp(complex(0, (k + G) * r)))
-                return tmp
-            Psi[i][k] = func
+                    tmp += C[i][k][G] * np.exp(complex(0, (k + G) * r))
+                Psi[i][k] = tmp
+        return Psi
 
+
+
+#################################
+# calculate electron density n(x)
+#################################
+def generate_plane_wave_basis_nik(r = None):
+    
+    global C, i_max, k_max, G_max
+    
+    Psi = [[None for k in range(k_max)]for i in range(i_max)]
+    
+    for i in range(i_max):
+        for k in range(k_max):            
+            tmp = ''
+            for G in range(G_max):                
+                for G_p in range(G_max):
+                    # print(C[i][k][G])
+                    tmp = tmp + " + " + f"{C[i][k][G]} * {C[i][k][G_p]} * np.exp(complex(0, {(k + G)} * r) - complex(0, {(k + G_p)} * r))"
+            Psi[i][k] = tmp
     return Psi
 
 
 
+nik = generate_plane_wave_basis_nik()
 
+
+
+"""
+    test nik
+"""
+r = 0.5
+eval(nik[0][0])
+
+
+
+# where k is node of reciperocal space
+# k가 1 ~ 9이면 node 순으로는 1번이 x = y = z = 0인 위치
+# reshape을 하면 첫번째 매트릭스부터 오른쪽으로 진행됨 ((0, 0), (0, 1), (0, 2), (1, 0)... )
+# 이 경우 r로 변환하면?
+
+############################
+# 각 node의 인덱스 생성
+############################
+nx= ny =nz = 3
+
+flattened_index_of_nodes = [None for i in range(nx * ny * nz)]
+
+q = 0
+for i in range(nx):
+    for j in range(ny):
+        for k in range(nz):
+            flattened_index_of_nodes[q] = (i, j, k)
+            q += 1
+
+
+
+
+
+"""
+    gernerate nodes
+"""
 
 # 1. solve poisson equation, get hartree potential (veff)
     # - create node
@@ -51,8 +135,7 @@ nodes = np.zeros((n_node_of_x, n_node_of_y, n_node_of_z))
     # create laplace operator, which is independent of basis functions 
     # create trial vector, we need the configuration of basis functions
 
-def generate_basis(nx, ny, nz): # flattened 3d array
-    return np.zeros((nx - 1) * (ny - 1) * (nz - 1))
+
 
 
 # # only  5 diagonal component is non-zero for 2d
